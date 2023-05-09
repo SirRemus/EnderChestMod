1fb878980e263b3542195a48f6244030366cc694
SirRemus <82674655+SirRemus@users.noreply.github.com>
2023-05-09 12:22:49 -0400

    Update EnderChestMod.java
    Added check to make the player need an ender chest within their inventory

--- src/main/java/net/sirremus/enderchest/EnderChestMod.java
+++ src/main/java/net/sirremus/enderchest/EnderChestMod.java
@@ -12,6 +12,8 @@ import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import static net.minecraft.server.command.CommandManager.literal;
+import net.minecraft.item.Items;
+import net.minecraft.item.ItemStack;
 
 public class EnderChestMod implements ModInitializer {
 
@@ -29,10 +31,24 @@ public class EnderChestMod implements ModInitializer {
     public static void openEnderChest(CommandContext<ServerCommandSource> context) {
         ServerPlayerEntity player = context.getSource().getPlayer();
         if (player != null) {
-            player.openHandledScreen(new SimpleNamedScreenHandlerFactory((i, playerInventory, playerEntity) ->
-                    new GenericContainerScreenHandler(ScreenHandlerType.GENERIC_9X3, i, player.getInventory(), player.getEnderChestInventory(), 3),
-                    Text.of("Ender Chest")
-            ));
+            if (hasEnderChestInInventory(player)) {
+                player.openHandledScreen(new SimpleNamedScreenHandlerFactory((i, playerInventory, playerEntity) ->
+                        new GenericContainerScreenHandler(ScreenHandlerType.GENERIC_9X3, i, player.getInventory(), player.getEnderChestInventory(), 3),
+                        Text.of("Ender Chest")
+                ));
+            } else {
+                player.sendMessage(Text.of("You must have an Ender Chest in your inventory to use this command."), false);
+            }
         }
+    }
+
+    private static boolean hasEnderChestInInventory(ServerPlayerEntity player) {
+        for (int i = 0; i < player.getInventory().size(); i++) {
+            ItemStack itemStack = player.getInventory().getStack(i);
+            if (itemStack.getItem() == Items.ENDER_CHEST) {
+                return true;
+            }
+        }
+        return false;
     }
 }
